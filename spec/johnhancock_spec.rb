e7898d07d5e3755cae9858c6cca86aebe01103dd
Ben Miller <bjmllr@gmail.com>
2016-07-02 10:44:06 -0400

    switch tests to spec style

---
+++ spec/johnhancock_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Johnhancock do
+  it 'has a version number' do
+    refute_nil ::Johnhancock::VERSION
+  end
+
+  it 'does something useful' do
+    assert true
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'johnhancock'
 
+require 'minitest/spec'
 require 'minitest/autorun'
