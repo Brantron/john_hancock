60761120db94a9af3eb3b78957468c67cf15d377
Ben Miller <bjmllr@gmail.com>
2016-07-02 11:06:49 -0400

    working out how to test this ...
    https://github.com/bootstrap-ruby/rails-bootstrap-forms/blob/master/test/bootstrap_form_test.rb

--- spec/johnhancock_spec.rb
+++ spec/johnhancock_spec.rb
@@ -1,11 +1,27 @@
 require 'spec_helper'
 
+module Johnhancock
+  module FormHelper
+    def signature_field
+      ''
+    end
+  end
+end
+
 describe Johnhancock do
   it 'has a version number' do
     refute_nil ::Johnhancock::VERSION
   end
 
-  it 'does something useful' do
-    assert true
+  describe '#signature_field' do
+    let(:view) do
+      Object.new.tap do |o|
+        o.extend Johnhancock::FormHelper
+      end
+    end
+
+    it 'returns a string' do
+      assert_equal String, view.signature_field.class
+    end
   end
 end
