616d5f1535bbb166e04bb093acdb8c8b1af6325a
Brandon Lawrence <brandon.lawrence@customink.com>
2016-07-04 20:29:25 -0400

    Prevent js errors when canvas isn't present

--- app/assets/javascript/john_hancock.js
+++ app/assets/javascript/john_hancock.js
@@ -1,14 +1,15 @@
 //= require signature_pad
 $( document ).ready(function() {
+  if(document.getElementById("JohnHancock-canvas")){
+    var canvas = document.getElementById("JohnHancock-canvas"),
+        hidden_field = document.getElementById("JohnHancock-hidden"),
+        parent_form = $(canvas).closest("form"),
+        signaturePad;
 
-  var canvas = document.getElementById("JohnHancock-canvas"),
-      hidden_field = document.getElementById("JohnHancock-hidden"),
-      parent_form = $(canvas).closest("form"),
-      signaturePad;
+    signaturePad = new SignaturePad(canvas);
 
-  signaturePad = new SignaturePad(canvas);
-
-  $(parent_form).submit(function(e) {
-    $(hidden_field).val(signaturePad.toDataURL());
-  });
+    $(parent_form).submit(function(e) {
+      $(hidden_field).val(signaturePad.toDataURL());
+    });
+  }
 })
