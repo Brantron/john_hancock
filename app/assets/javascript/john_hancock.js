940de13963126c1770b48f6930d7a2a4d0cd2f66
Brandon Lawrence <brandon.lawrence@customink.com>
2017-02-11 17:33:17 -0500

    s

--- app/assets/javascript/john_hancock.js
+++ app/assets/javascript/john_hancock.js
@@ -1,1 +1,16 @@
-//= require john_hancock/john_hancock
+//= require_tree .
+;(function($){
+  $( document ).ready(function() {
+    if(document.getElementById("JohnHancock-canvas")){
+      signaturePad = $("#JohnHancock-canvas")
+      hidden_field = document.getElementById("JohnHancock-hidden"),
+      parent_form = signaturePad.closest("form"),
+
+      signaturePad.jSignature()
+
+      $(parent_form).submit(function(e) {
+        $(hidden_field).val(signaturePad.jSignature("getData") );
+      });
+    }
+  })
+})(jQuery);
