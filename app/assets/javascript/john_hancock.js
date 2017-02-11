b4c09e0eae2c0fa435c91a5a531c0cefbd00208c
Brandon Lawrence <brandon.lawrence@customink.com>
2017-02-11 10:36:59 -0500

    move js assets

--- app/assets/javascript/john_hancock.js
+++ app/assets/javascript/john_hancock.js
@@ -1,15 +1,1 @@
-//= require jquery
-//= require signature_pad
-$( document ).ready(function() {
-  if(document.getElementById("JohnHancock-canvas")){
-    signaturePad = $("#JohnHancock-canvas")
-    hidden_field = document.getElementById("JohnHancock-hidden"),
-    parent_form = signaturePad.closest("form"),
-
-    signaturePad.jSignature()
-
-    $(parent_form).submit(function(e) {
-      $(hidden_field).val(signaturePad.jSignature("getData") );
-    });
-  }
-})
+//= require john_hancock/john_hancock
---
+++ app/assets/javascript/john_hancock/john_hancock.js
@@ -0,0 +1,17 @@
+//= require jquery
+//= require signature_pad
+;(function($){
+  $( document ).ready(function() {
+    if(document.getElementById("JohnHancock-canvas")){
+      signaturePad = $("#JohnHancock-canvas")
+      hidden_field = document.getElementById("JohnHancock-hidden"),
+      parent_form = signaturePad.closest("form"),
+
+      signaturePad.jSignature()
+
+      $(parent_form).submit(function(e) {
+        $(hidden_field).val(signaturePad.jSignature("getData") );
+      });
+    }
+  })
+})(jQuery);
